192f4ec80f2bdb27b97515b0bb586553e58cd3c6
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-04-25 22:44:59 +0200

    Use layout for manual
    - Fix css while resizing windows

--- js/sidebar.js
+++ js/sidebar.js
@@ -4,21 +4,46 @@ const sidewrapclass='.sidebar-container'
 const navbarclass='.top-bar'
 const headerbarclass = '.header'
 const mainclass='.main'
+const footerclass='footer'
 
 const navbarheight = $( navbarclass ).height();
 const fixmeTop = $( sideclass ).position().top;
 
 
-//var wins = $( window ).width();
-//$( sideclass ).css({width: wins*0.2});
-//$( sidewrapclass ).css({width: wins*0.2});
-//
-//$(window).resize(function() { 
-//    var wins = $( window ).width();
-//    $( sideclass ).css({width: wins*0.2});
-//    $( sidewrapclass ).css({width: wins*0.2});
-//});
+var rmSideBar = function() {
+    var winw = $( window ).width();
+    if( winw < 1000 ) {
+        $( sidewrapclass ).css({
+            display: 'none'
+        });
+        $( mainclass ).css({
+            width: '100%'
+        });
+        $( footerclass ).css({
+            width: '100%'
+        });
+    }
+    else {
+        $( sidewrapclass ).css({
+            display: 'initial'
+        });
+        $( mainclass ).css({
+            width: '80%'
+        });
+        $( footerclass ).css({
+            width: '80%'
+        });
+    }
+}
 
+rmSideBar();
+
+// Remove side bar for small window.
+$(window).resize(function() { 
+    rmSideBar();
+});
+
+// Stick sidebar to navbar.
 $(window).scroll(function() {                  
     var currentScroll = $(window).scrollTop(); 
     if( currentScroll >= fixmeTop )
@@ -33,3 +58,6 @@ $(window).scroll(function() {
         });
     }
 });
+
+
+
