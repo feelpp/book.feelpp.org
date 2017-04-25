d83c52e1c2316b4b2f530341f9156c0106d22138
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-04-25 20:44:57 +0200

    Update css + clusters

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,19 +1,34 @@
 // Fix the sidebar under navigation bar.
 const sideclass='.sidebar'
+const sidewrapclass='.sidebar-container'
 const navbarclass='.top-bar'
-var fixmeTop = $( sideclass ).position().top;       // get initial position of the element
-var navbarheight = $( navbarclass ).height();
-$(window).scroll(function() {                  // assign scroll event listener
-    var currentScroll = $(window).scrollTop(); // get current position
+const headerbarclass = '.header'
+const mainclass='.main'
 
-    if (currentScroll >= fixmeTop) {           // apply position: fixed if you
-        $(sideclass).css({                      // scroll to that element or below it
+const navbarheight = $( navbarclass ).height();
+const fixmeTop = $( sideclass ).position().top;
+
+
+//var wins = $( window ).width();
+//$( sideclass ).css({width: wins*0.2});
+//$( sidewrapclass ).css({width: wins*0.2});
+//
+//$(window).resize(function() { 
+//    var wins = $( window ).width();
+//    $( sideclass ).css({width: wins*0.2});
+//    $( sidewrapclass ).css({width: wins*0.2});
+//});
+
+$(window).scroll(function() {                  
+    var currentScroll = $(window).scrollTop(); 
+    if( currentScroll >= fixmeTop )
+    {           
+        $(sideclass).css({                    
             position: 'fixed',
-            top: navbarheight,
-            left: '0',
+            top: navbarheight
         });
-    } else {                                   // apply position: static
-        $(sideclass).css({                      // if you scroll above it
+    } else {                                  
+        $(sideclass).css({                    
             position: 'static'
         });
     }
