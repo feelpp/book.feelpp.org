9a6c4fb273c04094c806bfe5b36fd274e40ee486
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-04-27 23:46:21 +0200

    Reorganize directories and clean files headers

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,47 +1,10 @@
 // Fix the sidebar under navigation bar.
 const sideclass='.sidebar'
-const sidewrapclass='.sidebar-container'
 const navbarclass='.top-bar'
 const headerbarclass = '.header'
-const mainclass='.main'
-const footerclass='footer'
 
 const navbarheight = $( navbarclass ).height();
 const fixmeTop = $( sideclass ).position().top;
-
-
-var rmSideBar = function() {
-    var winw = $( window ).width();
-    if( winw < 1000 ) {
-        $( sidewrapclass ).css({
-            display: 'none'
-        });
-        $( mainclass ).css({
-            width: '100%'
-        });
-        $( footerclass ).css({
-            width: '100%'
-        });
-    }
-    else {
-        $( sidewrapclass ).css({
-            display: 'initial'
-        });
-        $( mainclass ).css({
-            width: '80%'
-        });
-        $( footerclass ).css({
-            width: '80%'
-        });
-    }
-}
-
-rmSideBar();
-
-// Remove side bar for small window.
-$(window).resize(function() { 
-    rmSideBar();
-});
 
 // Stick sidebar to navbar.
 $(window).scroll(function() {                  
@@ -58,6 +21,3 @@ $(window).scroll(function() {
         });
     }
 });
-
-
-
