68f60a260895bfa2a6856a64e01172dc2feb265d
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-07-19 14:41:01 +0200

    allow build and deploy on different agents :)

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -7,3 +7,5 @@ BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 
 echo "--- Building $PROJECT..."
 jekyll build
+tar czf site.tar.gz _site/
+buildkite-agent artifact upload site.tar.gz
--- .buildkite/steps/deploy.sh
+++ .buildkite/steps/deploy.sh
@@ -6,4 +6,5 @@ set -x
 BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 
 echo "--- Deploying $PROJECT..."
+buildkite-agent artifact download site.tar.gz . --build ${BUILDKITE_BUILD_ID}
 rsync -avz _site/ $WEBSERVER:~/public_html/${PROJECT}
