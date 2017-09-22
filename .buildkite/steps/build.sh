f02bd21cf5141ba721baf1c26e23ecd68a6a433b
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-09-22 08:55:47 +0200

    update

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -6,6 +6,8 @@ set -x
 BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 
 echo "--- Building $PROJECT..."
+bundle config --local github.https true
+bundle --path=.bundle/gems --binstubs=.bundle/.bin
 jekyll build
 tar czf site.tar.gz _site/
 buildkite-agent artifact upload site.tar.gz
