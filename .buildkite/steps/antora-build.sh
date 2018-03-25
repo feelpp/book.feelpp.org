786de8655c605df3ae43bc9c47f385f248beab4f
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-03-25 19:32:54 +0200

    up #47
    try fix antora build

--- .buildkite/steps/antora-build.sh
+++ .buildkite/steps/antora-build.sh
@@ -5,4 +5,4 @@ set -x
 
 BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 echo "--- Building $PROJECT..."
-cd antora && antora --html-url-extension-style=indexify antora-github-feelpp-doc.yml
+antora --html-url-extension-style=indexify antora-github-feelpp-doc.yml
