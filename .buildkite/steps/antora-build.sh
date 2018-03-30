10c54d33b0979a6d0071a60fd57812ab58665364
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-03-30 08:45:00 +0200

    exclude images for now until antora git-lfs is fixed #47

--- .buildkite/steps/antora-build.sh
+++ .buildkite/steps/antora-build.sh
@@ -8,5 +8,5 @@ echo "--- Building $PROJECT..."
 antora --html-url-extension-style=indexify antora-github-feelpp-doc.yml
 ls -lrta
 
-tar czf site.tar.gz build
+tar  --exclude='*.png' --exclude="*.jpg" --exclude="*.jpeg" -c -z -f site.tar.gz build
 buildkite-agent artifact upload site.tar.gz --job $BUILDKITE_JOB_ID
