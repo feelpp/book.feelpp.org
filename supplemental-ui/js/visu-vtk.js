e00fac1ae7f81759284e8dcd206d02a4b5415606
Vincent Chabannes <vincent.chabannes@feelpp.org>
2018-04-06 16:38:23 +0200

    up visu-vtk.js

--- supplemental-ui/js/visu-vtk.js
+++ supplemental-ui/js/visu-vtk.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fce0974d46d1d2496825ac64ae21fc534d18865ac65822eaa5e31a87f33b021
-size 686117
+oid sha256:d54ad2bf91b5189ff34bf7d81aa4cb48c71c00afaa10ec6f2921b83c1b561752
+size 686110
