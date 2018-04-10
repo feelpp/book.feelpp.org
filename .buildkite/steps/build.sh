90835ee9026799b3b247f1263611016f7d7ab789
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-04-10 07:56:56 +0200

    up book
    fix images in the pipeline

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -6,8 +6,4 @@ set -x
 BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 
 echo "--- Building $PROJECT..."
-bundle config --local github.https true
-bundle --path=.bundle/gems --binstubs=.bundle/.bin
-bundle exec jekyll build
-tar czf site.tar.gz _site/
-buildkite-agent artifact upload site.tar.gz
+make sync
