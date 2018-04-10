e3561b8a881a4811000f246f99a8b44b8145fd46
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-04-11 01:23:13 +0200

    add nvm

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -6,4 +6,7 @@ set -x
 BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 
 echo "--- Building $PROJECT..."
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 make sync
