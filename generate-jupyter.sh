27524cd039a6da09544d79b6f658c60e1cb66f35
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-11-03 23:35:22 +0100

    up

--- generate-jupyter.sh
+++ generate-jupyter.sh
@@ -8,6 +8,8 @@ for i in $(find $dir -name "*.adoc" | grep pages ); do
         dir=$(dirname $ipynb)
         mkdir -p jupyter/$dir
         echo "generating jupyter/$dir/$(basename $ipynb .adoc).ipynb..."
+        pwd
+        ls node_modules/.bin
         node_modules/.bin/asciidoctor -r asciidoctor-jupyter -b jupyter $i -o jupyter/$dir/$(basename $ipynb .adoc).ipynb
     fi; 
 done
